5b96219952369b746a0f760570460134e252868b
Nikhil Mohanty <69157165+Nikhil15Mohanty@users.noreply.github.com>
2023-08-19 17:34:10 +0530

    Update chaiaurcode.js

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,16 @@
-console.log('Project 5');
+const insert = document.getElementById('insert');
+window.addEventListener('keydown',(e)=>{
+  insert.innerHTML =`<table>
+  <tr>
+    <th>Key</th>
+    <th>Code</th>
+    <th>KeyCode</th>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.code}/td>
+    <td>${e.keyCode}/td>
+  </tr>
+</table>`
+})
+
